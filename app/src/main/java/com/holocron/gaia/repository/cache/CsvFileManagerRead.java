67b05dcca52ed3a4174d3c0298c0f60e9e56b607
Jhoanes Freitas <jhoanesbarbosa@gmail.com>
2015-08-06 20:36:06 -0300

    Implementadoa leitura do arquivo .cvs

---
+++ app/src/main/java/com/holocron/gaia/repository/cache/CsvFileManagerRead.java
@@ -0,0 +1,76 @@
+package com.holocron.gaia.repository.cache;
+
+import com.googlecode.jcsv.reader.CSVReader;
+import com.googlecode.jcsv.reader.internal.CSVReaderBuilder;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by jhoanes on 05/08/15.
+ */
+public class CsvFileManagerRead {
+
+    public String meatType(int day, boolean meatType) throws IOException {
+
+        String[] stringLunch = new String[5];
+        String[] stringDinner = new String[5];
+
+        Reader reader = new FileReader("/storage/sdcard0/Cardápio - cantina IFRN - Página1(1).csv");
+
+        CSVReader<String[]> csvMeatReader = CSVReaderBuilder.newDefaultReader(reader);
+
+        List<String[]> card = csvMeatReader.readAll();
+
+        List<String> lunchWeek = Arrays.asList(card.get(3)[0].split(","));
+
+        List<String> dinnerWeek = Arrays.asList(card.get(6)[0].split(","));
+
+        //Fazer nova classe para retornar apenas a string;
+        if (meatType) {
+
+            int aux = 0;
+
+            for (String meat : lunchWeek) {
+
+                stringLunch[aux] = meat;
+
+                aux++;
+
+            }
+
+            for (int i = 0; i < 5; i++){
+
+                if (day == i)
+                    return stringLunch[i];
+
+            }
+
+
+        } else {
+
+            int aux = 0;
+
+            for (String meat : dinnerWeek) {
+
+                stringDinner[aux] = meat;
+
+                aux++;
+
+            }
+
+            for (int i = 0; i < 5; i++){
+
+                if (day == i)
+                    return stringDinner[i];
+
+            }
+        }
+        return "Arquivo não encontrado!";
+    }
+}
