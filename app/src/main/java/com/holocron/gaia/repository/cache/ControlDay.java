cc8a3882df48af9050747dfb219a9e7c1a789fb8
Jhoanes Freitas <jhoanesbarbosa@gmail.com>
2015-08-08 15:07:38 -0300

    Criacao da classe ControlDay com um metodo data que retorna um string

---
+++ app/src/main/java/com/holocron/gaia/repository/cache/ControlDay.java
@@ -0,0 +1,34 @@
+package com.holocron.gaia.repository.cache;
+
+import com.googlecode.jcsv.reader.CSVReader;
+import com.googlecode.jcsv.reader.internal.CSVReaderBuilder;
+import com.holocron.gaia.Constants;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by jhoanes on 08/08/15.
+ */
+public class ControlDay {
+
+    public String Data() throws IOException {
+
+        Reader reader = new FileReader(Constants.FILENAME);
+
+        CSVReader<String[]> csvMeatReader = CSVReaderBuilder.newDefaultReader(reader);
+
+        List<String[]> card = csvMeatReader.readAll();
+
+        List<String> weekDay = Arrays.asList(card.get(1)[0]);
+
+        for (String retorn: weekDay)
+            return retorn;
+
+        return null;
+    }
+}
